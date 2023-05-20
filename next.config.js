5f70ddfbcd7fd83c05f6c6367af55f5d9c8dc3dd
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-20 11:53:33 +0200

    soon to deploy done

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const withPWA = require("next-pwa")({
 const nextConfig = {
     env: {
         key: process.env.COUNT_API_KEY,
-        URL: "https://animevariant-com-api.vercel.app/api/",
+        URL: "http://host.docker.internal:3200/api/",
         GA_MEASUREMENT_ID: "G-3D40VC4QXL",
     },
     images: {
